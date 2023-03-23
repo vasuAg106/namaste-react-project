66f29a1ad38bca755d615c8a0954ccb2deb30359
vasuAg106 <vasundhra.ag106@gmail.com>
2023-03-23 22:36:44 +0530

    Episode 03

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
-/**
- * <div id="parent">
- *      <div id="child">
- *          <h1></h1>
- *          <h2></h2>
- *      </div>
- * </div>
- */
-
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 
 const parent = React.createElement("div", {id:"parent"}, 
     React.createElement("div" , {id:"child"}, 
         [ React.createElement("h1", {}, "Nested loop"), React.createElement("h2", {}, "Sibling")]
     )
 )
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from react");
-console.log(heading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
+
 root.render(parent);
